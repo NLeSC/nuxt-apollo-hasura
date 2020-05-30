f1c52eeb647951833f2c4c38fae22464d084da89
Jesús García <ctw@ctwhome.com>
2020-05-30 11:19:31 +0200

    Add headers to apollo provider with valid token

--- apollo/clientConfig.js
+++ apollo/clientConfig.js
@@ -1,16 +1,20 @@
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
+const getHeaders = () => {
+  const headers = {}
+  const token = localStorage.getItem('auth._token.auth0')
+  if (token !== 'false') {
+    headers.authorization = token
+  }
+  return headers
+}
+
 export default function (context) {
   return {
     httpLinkOptions: {
       uri: process.env.baseUriHasura,
       credentials: 'same-origin',
-      // headers: getHeaders(),
-    },
-    getAuth: () => {
-      // get the authentication token from local storage if it exists
-      const token = localStorage.getItem('auth._token.auth0')
-      return !token ? token : ''
+      headers: getHeaders(),
     },
     cache: new InMemoryCache(),
     wsEndpoint: process.env.baseWsHasura,
