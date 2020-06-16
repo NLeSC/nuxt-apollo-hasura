632ee49dbecad228f6c04700a32f18bba58d64d6
Pushpanjali Pawar <pushpanjalip55@gmail.com>
2020-06-16 14:42:53 +0200

    Created video player component

---
+++ pages/erd.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="erd">
+    <VideoPlayer src="video/erd_video.mp4"></VideoPlayer>
+  </div>
+</template>
+
+<script>
+import VideoPlayer from "../components/VideoPlayer";
+
+export default {
+  name: "Erd",
+  components: {
+    VideoPlayer
+  }
+};
+</script>
+
+<style>
+#erd {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  width: 500px;
+  height: 500px;
+}
+</style>
