1f5ae18e4b19ffaa37e842b79ee0d5603d2cf5bf
Berend Weel <b.weel@esciencecenter.nl>
2020-06-17 14:38:39 +0200

    Added directory selection input and list of videos

--- pages/videos.vue
+++ pages/videos.vue
@@ -4,11 +4,28 @@
       <v-card-title class="headline">
         Videos
       </v-card-title>
-      <input type="file" id="videodir" webkitdirectory directory multiple/>
+    <input type="file" id="videodir" v-on:change="filesChanged($event.target.name, $event.target.files);" webkitdirectory directory multiple  />
+    <div v-for="video in videos">
+      {{video}}
+    </div>
     </v-card>
   </div>
 </template>
 <script>
 export default {
+  data() {
+    return {
+      videos: [],
+    }
+  },
+  methods: {
+    filesChanged(name, files) {
+      for (const file of files) {
+          if (file.type.startsWith('video')) {
+            this.videos.push(file.name)
+          }
+      }
+    }
+  }
 }
 </script>
