dda5701138358b797855828a9caea3207e2e3e53
Pushpanjali Pawar <pushpanjalip55@gmail.com>
2021-02-24 10:58:31 +0100

    Fethed video metadata from database with video hash

--- components/tabs/Info.vue
+++ components/tabs/Info.vue
@@ -1,19 +1,46 @@
 <template>
   <v-container>
-    <div class="headline">Participant {{ db_video.participant }}</div>
-    <p>study: {{ db_video.study }}</p>
-    <p>Session: {{ db_video.session }}</p>
-    <p>Memory Type: {{ db_video.memory_type }}</p>
-    <p>Memory Index: {{ db_video.memory_index }}</p>
+    <div class="headline">Participant {{ video_metadata.participant }}</div>
+    <p>study: {{ video_metadata.study }}</p>
+    <p>Session: {{ video_metadata.session }}</p>
+    <p>Memory Type: {{ video_metadata.memory_type }}</p>
+    <p>Memory Index: {{ video_metadata.memory_index }}</p>
   </v-container>
 </template>
 
 <script>
+import get_video_metadata from '~/apollo/get_video_metadata'
 export default {
   data() {
     return {
-      db_video: {}, // Todo
+      video_metadata: {},
     }
+  },
+  computed: {
+    video_hash() {
+      // Retrieve selected video from vuex store
+      const selectedVideo = this.$store.state.videos.selectedVideo
+      return selectedVideo.hash
+    },
+  },
+  apollo: {
+    videoMetadata: {
+      variables() {
+        return {
+          hash: this.video_hash,
+        }
+      },
+      query: get_video_metadata,
+      result({ data }) {
+        if (data) {
+          this.video_metadata = data.videoMetadata[0]
+        }
+      },
+      error(error) {
+        this.error = JSON.stringify(error.message)
+        console.error('Error', this.error)
+      },
+    },
   },
 }
 </script>
