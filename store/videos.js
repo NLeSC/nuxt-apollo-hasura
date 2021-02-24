f5489a06f0ff8be41b1eb13f3a7411a71f286743
Pushpanjali Pawar <pushpanjalip55@gmail.com>
2021-02-24 14:48:58 +0100

    removed eslint comment

--- store/videos.js
+++ store/videos.js
@@ -1,16 +1,15 @@
-/* eslint-disable prettier/prettier */
 export const state = () => {
-    return {
-        selectedVideo: {}
-    }
+  return {
+    selectedVideo: {},
+  }
 }
 export const mutations = {
-    SELECT_VIDEO(state, payload) {
-        state.selectedVideo = payload || {}
-    },
+  SELECT_VIDEO(state, payload) {
+    state.selectedVideo = payload || {}
+  },
 }
 export const actions = {
-    selectVideo(context, video) {
-        context.commit("SELECT_VIDEO", video)
-    }
+  selectVideo(context, video) {
+    context.commit('SELECT_VIDEO', video)
+  },
 }
